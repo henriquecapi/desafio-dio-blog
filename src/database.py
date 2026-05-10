f4ed435315e421163b39cefa4210fe1a7480e8e6
henriquecapi <henriquecapi@hotmail.com>
2026-05-10 01:01:19 -0300

    fix: remove check_same_thread for non-sqlite databases

--- src/database.py
+++ src/database.py
@@ -6,9 +6,11 @@ from config import settings
 database = databases.Database(settings.database_url)
 metadata = sa.MetaData()
 
-if settings.environment == "production":
-    engine = sa.create_engine(settings.database_url)
-else:
+# Verifica se o banco é SQLite para aplicar argumentos específicos
+if settings.database_url.startswith("sqlite"):
     engine = sa.create_engine(
         settings.database_url, connect_args={"check_same_thread": False}
     )
+else:
+    # Para Postgres e outros, não usamos check_same_thread
+    engine = sa.create_engine(settings.database_url)
